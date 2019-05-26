ec4d4581ce811555a12691e0e4403c0ae80d076d
skalex05 <skalex05@gmail.com>
2019-05-26 23:01:09 +0100

    V1.1

--- 3D Camera Examples/Assets/Dynamic Follow/DynamicFollow.cs
+++ 3D Camera Examples/Assets/Dynamic Follow/DynamicFollow.cs
@@ -9,16 +9,14 @@ public class DynamicFollow : MonoBehaviour
     public Vector3 TargetPositionOffset;
     Quaternion desiredRotation;
     public float Sensitivity;
-    [Range(0,1)]public float rotationSmoothing;
+    [Range(0, 1)] public float rotationSmoothing;
     [Range(0, 20)] public float maxDistanceToTarget;
-    [Range(0, 360)] public float maxRotation;
 
     Vector3 targetPositionOffset;
 
     // Start is called before the first frame update
     void Start()
     {
-        targetPositionOffset = TargetPositionOffset;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -27,29 +25,35 @@ public class DynamicFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-
-        ROT = ROT +  new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Sensitivity;
-        desiredRotation = Quaternion.Euler(new Vector3(Mathf.Clamp(ROT.x,-viewLimit,viewLimit),ROT.y,ROT.z));
+        ROT = ROT + new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0) * Sensitivity;
+        ROT = new Vector3(Mathf.Clamp(ROT.x, -viewLimit, viewLimit), ROT.y, ROT.z);
+        desiredRotation = Quaternion.Euler(ROT);
 
         Vector3 newrot = Quaternion.Lerp(transform.rotation, desiredRotation, rotationSmoothing).eulerAngles;
         transform.rotation = Quaternion.Euler(newrot);
         transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
 
         float distance = maxDistanceToTarget;
-        
-        if (Physics.Raycast(target.position + targetPositionOffset.x * target.right + target.forward * targetPositionOffset.z + new Vector3(0, targetPositionOffset.y, 0), -transform.forward,out RaycastHit hit,maxDistanceToTarget))
+
+        if (Physics.Raycast(target.position + TargetPositionOffset.x * transform.right + transform.forward * TargetPositionOffset.z + new Vector3(0, TargetPositionOffset.y, 0), -transform.forward, out RaycastHit hit, maxDistanceToTarget))
         {
             distance = hit.distance - 0.05f;
         }
-        if (Physics.SphereCastAll(target.position,0.001f,targetPositionOffset.x * target.right + target.forward * targetPositionOffset.z + new Vector3(0, targetPositionOffset.y, 0)).Length >= 2)
+        if (Physics.OverlapSphere(target.position + TargetPositionOffset.x * transform.right + transform.forward * TargetPositionOffset.z + new Vector3(0, TargetPositionOffset.y, 0), 0.001f).Length >= 1)
         {
-            targetPositionOffset = Vector3.Lerp(targetPositionOffset,Vector3.zero,rotationSmoothing);
+            foreach (Collider c in Physics.OverlapSphere(target.position + TargetPositionOffset.x * transform.right + transform.forward * TargetPositionOffset.z + new Vector3(0, TargetPositionOffset.y, 0), 0.001f))
+            {
+                if (c.gameObject == target)
+                {
+                    break;
+                }
+            }
+            targetPositionOffset = Vector3.Lerp(targetPositionOffset, Vector3.zero, rotationSmoothing);
         }
         else
         {
-            targetPositionOffset = Vector3.Lerp(targetPositionOffset,TargetPositionOffset,rotationSmoothing);
+            targetPositionOffset = Vector3.Lerp(targetPositionOffset, TargetPositionOffset, rotationSmoothing);
         }
-        transform.position = target.position + targetPositionOffset.x * target.right + target.forward * targetPositionOffset.z + new Vector3(0,targetPositionOffset.y,0)  + -transform.forward * distance;
+        transform.position = target.position + targetPositionOffset.x * transform.right + transform.forward * targetPositionOffset.z + new Vector3(0, targetPositionOffset.y, 0) + -transform.forward * distance;
     }
 }
